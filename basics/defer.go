67814da9ad3f5518bb36dacd0a0a906cf84591b8
oscar.sanguino <oscar.sanguino@contractor.vroom.com>
2023-02-15 18:48:10 -0500

    Creating method using defer in the logic

--- basics/defer.go
+++ basics/defer.go
@@ -1,4 +1,43 @@
 package basics
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 // testDefer Simple test to check the defer method
-func testDefer() {}
+func testDefer() {
+	filename := "testFile.txt"
+	createdError := createFile(filename, "testData")
+	deletedError := deleteFile(filename)
+	fmt.Println("Creating file ", filename, " with error: ", createdError, " and deleted with error: ", deletedError)
+}
+
+func createFile(filename string, data interface{}) (err error) {
+	newFile, err := os.Create(filename)
+
+	if err != nil {
+		return
+	}
+
+	// using defer to close the file after finishing with the script
+	defer newFile.Close()
+
+	dataBytes, err := json.Marshal(data)
+
+	if err != nil {
+		return
+	}
+
+	defer newFile.Sync()
+	newFile.Write(dataBytes)
+
+	return
+}
+
+func deleteFile(filename string) error {
+	// deleting the file after finishing with it
+	err := os.Remove(filename)
+	return err
+}
