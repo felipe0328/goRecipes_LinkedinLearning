6d38d1b8fbd4c20492b0a8bb29971c356e63deb2
Felipe Puentes <feliipepuentes@gmail.com>
2023-02-20 07:14:58 -0500

    adding info to empty interface

--- structs/emptyInterfaces.go
+++ structs/emptyInterfaces.go
@@ -1,5 +1,6 @@
 package structs
 
 // This script will show what is the purpose to work with empty interfaces
+// The only interface should be used only as last resouse
 
 func testEmptyInterfaces() {}
